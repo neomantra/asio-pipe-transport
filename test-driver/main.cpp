99032a3e7134eda9e0f5861c1212d9f21eb363f0
Peter Thorson <git@zaphoyd.com>
2015-01-15 07:34:56 -0600

    update list of tests to write

--- test-driver/main.cpp
+++ test-driver/main.cpp
@@ -15,11 +15,16 @@
 // listen, connect, send/recv in both directions
 
 // listen when there is already a socket file
+// listen when the path is bogus
 
 // connect when nothing is listening
 
 // read/write before opening
 // read/write after close
+
+// accept two connections with one acceptor
+// accept while already accepting
+// accept connection with already connected endpoint
 
 
 int main(int argc, const char * argv[]) {
